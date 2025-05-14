165247713dc003a614530b0608b57384fd8c60ce
KingVizName <saitkulovbulat2@gmail.com>
2025-05-14 20:01:25 +0500

    gg

--- second_win.py
+++ second_win.py
@@ -1,3 +1,4 @@
+from instr import*
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit, QWidget, QLabel, QVBoxLayout, QHBoxLayout
 
@@ -17,9 +18,50 @@ class secondWin(QWidget):
         self.hLay.addLayout(self.lLay)
         self.hLay.addLayout(self.rLay)
 
-        self.widget.setLayout(hLay)
+        self.setLayout(hLay)  
 
-
+        self.name = QLabel(txt_name)
+        self.enterName = QLineEdit(txt_hintname)
+        self.age = QLabel(txt_age)
+        self.enterAge = QLineEdit(txt_hintage)
+        self.test1 = QLabel(txt_test1)
+        self.button1 = QPushButton()
+        self.enterTest1 = QLineEdit(txt_hinttest1)
+        self.test2 = QLabel(txt_test2)
+        self.button2 = QPushButton()
+        self.enterTest2 = QLineEdit(txt_hinttest2)
+        self.test3 = QLabel(txt_test3)
+        self.button3 = QPushButton()
+        self.enterTest31 = QLineEdit()
+        self.enterTest32 = QLineEdit()
+        self.timer = QLabel()
+        
+        self.buttonNext = QPushButton()
+    
+        self.rLay.addWidget(self.enterTest32, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.enterTest31, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.test3, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.enterTest2, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.button2, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.test2, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.enterTest1, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.button1, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.test1, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.enterAge, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.age, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.enterName, alignment= Qt.AlignCenter)
+        self.rLay.addWidget(self.name, alignment= Qt.AlignCenter)
+        self.lLay.addWidget(self.timer, alignment= Qt.AlignCenter)
 
         
 
+    def connets(self):
+        self.buttonNext.clicked.connect(self.next_click)
+    
+    def next_click(self):
+        self.hide()
+        self.tw = finalWin()
+
+app = QApplication([])
+scndwn = SecondWin()
+app.exec_()
